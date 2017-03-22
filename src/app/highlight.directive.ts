d6ce8ba81fd1e72a993b51ff6f652ae2f66f2228
Ariel <ariel.kuzminski@gmail.com>
2017-03-22 12:47:58 +0100

    Property Binding

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,10 +1,24 @@
-import { Directive } from '@angular/core';
+import {Directive, HostListener, HostBinding} from '@angular/core';
 
 @Directive({
   selector: '[dirHighlight]'
 })
 export class HighlightDirective {
+  @HostListener('mouseenter') mouseover() {
+    this.backgroundColor = 'green';
+  };
 
-  constructor() { }
+  @HostListener('mouseleave') mouseleave() {
+    this.backgroundColor = 'white';
+  };
 
+  @HostBinding('style.backgroundColor') get setColor() {
+    return this.backgroundColor;
+  };
+
+  private backgroundColor = 'white';
+  constructor() {
+    //this.elementRef.nativeElement.style.backgroundColor = 'green';
+    //this.renderer.setElementStyle(this.elementRef.nativeElement, 'background-color', 'green');
+  }
 }
